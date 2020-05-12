731ba35c3f81a7971e6d48bc2e2b81d99aba79a1
Luis Angel Ortega <angel-ortega12@hotmail.com>
2020-05-12 12:48:57 -0600

    🗃 Se añade la vista de los datos en general
    Se añade la vista de los datos en general

---
+++ database/migrations/2020_05_12_020953_overall_view.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OverallView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE 
+            ALGORITHM = UNDEFINED 
+        VIEW `schola`.`vistaGeneral` AS
+            SELECT 
+                `r`.`idMaestroCurso` AS `idMaestroCurso`,
+                `cu`.`nombreCurso` AS `nombreCurso`
+                `d`.`idMaestro` AS `idMaestro`,
+                `d`.`nombre` AS `nombreMaestro`,
+                `d`.`paterno` AS `paternoMaestro`,
+                `d`.`materno` AS `maternoMaestro`,
+                `d`.`matricula` AS `matriculaMaestro`,
+                `c`.`gradoAcademico` AS `gradoAcademico`,
+            FROM
+                (((`schola`.`catGradoAcademico` `c`
+                JOIN `schola`.`datMaestro` `d` ON ((`c`.`idGradoAca` = `d`.`idGradoAca`)))
+                JOIN `schola`.`relMaestroCurso` `r` ON ((`d`.`idMaestro` = `r`.`idMaestro`)))
+                JOIN `schola`.`catCursos` `cu` ON ((`r`.`idCurso` = `cu`.`idCurso`)))
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB:unprepared('DROP VIEW IF EXISTS vistaGeneral;');
+    }
+}
